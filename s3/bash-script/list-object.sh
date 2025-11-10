15448e2a91f0fa31efaaa7fb105a416facb14182
jenish-chauhan <chauhanjenish@gmail.com>
2025-11-10 23:37:33 +0530

    just practiced

---
+++ s3/bash-script/list-object.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# This script creates an S3 bucket in AWS.
+
+echo "=== list bucket ==="
+
+# Check for the bucket name
+if [ -z "$1" ]; then
+  echo "Bucket name not provided. Usage: ./create-bucket.sh <bucket-name>"
+  exit 1
+fi
+
+Bucket_name=$1
+
+# Create the bucket (replace us-east-1 if needed)
+aws s3api list-bucket-v2 \
+  --bucket "$Bucket_name" \
+   
+ 
+
+echo "Bucket '$Bucket_name' deleted successfully."
